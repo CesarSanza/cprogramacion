e487d85bf3e8b5075b819b1fe2f95b10915abada
CesarSanza <cesar.sanchezar@outlook.com>
2026-02-04 14:58:08 -0600

    subi los archivos del dia 28 de enero

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿int variable1;
+int resultado;
+
+static void chamba(int variable1)
+{ 
+    int resultado = variable1 % 2;
+    
+    if (resultado == 0)
+        Console.WriteLine("El numero es par");
+    else
+        Console.WriteLine("El numero es impar");
+}
+Console.WriteLine("Dame un numero:");
+variable1 = Convert.ToInt32(Console.ReadLine());
+chamba(variable1);
---
+++ Programa si es para o impar/Programa si es para o impar/Program.cs
@@ -0,0 +1,21 @@
+﻿int number1;
+int residuo;
+
+static void funcion(int number1)
+{
+    int residuo = number1 % 2;
+    if (residuo == 0)
+        Console.WriteLine("The number its par");
+    else
+        Console.WriteLine("The number its impar");
+}
+Console.WriteLine("Give me a number");
+string input = Console.ReadLine();
+number1 = int.Parse(input);
+
+for (int i = 0; i <= 10; i++)
+{
+    int resultado = number1 * i;
+    Console.WriteLine(number1 + " x " + i + " = " + resultado);
+}
+funcion(number1);
