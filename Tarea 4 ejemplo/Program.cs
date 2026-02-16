3a0543749945257fdb5436670e05d29f76092c4b
CesarSanza <cesar.sanchezar@outlook.com>
2026-02-16 17:21:46 -0600

    Tarea 4

---
+++ Tarea 4 ejemplo/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Dame un número:");
+int numero = Convert.ToInt32(Console.ReadLine());
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El número es par.");
+}
+else
+{
+    Console.WriteLine("El número es impar.");
+}
