5069cf9d5ba046fb8ce3eec0a89eac37b28e697f
CesarSanza <cesar.sanchezar@outlook.com>
2026-02-20 13:42:04 -0600

    Actividad miercoles

---
+++ Book/Book/Libro.cs
@@ -0,0 +1,38 @@
+﻿namespace Book;
+
+public class Libro
+{
+    private string title;
+    private string author;
+    private int pages;
+    private bool open;
+    private bool closed;
+    public Libro()
+    {
+        title = "Big Boss";
+        author = "Charlie";
+        pages = 200;
+        open = false;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Author: " + author);
+        Console.WriteLine("Pages: " + pages);
+
+    }
+    public void Open()
+    {
+        open = true;
+        Console.WriteLine("El libro \"" + title + "\"se abrio.");
+    }
+
+    public void Close() 
+    {
+        open = false;
+        Console.WriteLine("El libro \"" + title + "\" se cerro.");
+    }
+}
+
+
---
+++ Book/Book/Program.cs
@@ -0,0 +1,12 @@
+﻿using Book;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        Libro myBook = new Libro();
+        myBook.ShowInfo();
+        myBook.Open();               
+        myBook.Close();
+    }
+}
