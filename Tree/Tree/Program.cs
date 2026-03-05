d0052b4be7e12e8a6391d7b70ef2e8b3f8166202
CesarSanza <cesar.sanchezar@outlook.com>
2026-03-05 12:24:25 -0600

    Tarea del lunes

---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿using TreeNamespace;
+
+Tree tree = new Tree();
+
+Console.WriteLine("=== Tree Simulator ===");
+Console.WriteLine("Commands: bloom | dry | exit");
+Console.WriteLine("Size: " + tree.size + " | Foliage: " + tree.foliage);
+Console.WriteLine("----------------------");
+
+while (true)
+{
+    Console.Write("Enter command: ");
+    string input = Console.ReadLine().Trim().ToLower();
+
+    if (input == "exit")
+    {
+        Console.WriteLine("Exiting...");
+        break;
+    }
+    else if (input == "bloom")
+    {
+        tree.Bloom();
+    }
+    else if (input == "dry")
+    {
+        tree.Dry();
+    }
+    else
+    {
+        Console.WriteLine("Unknown command. Use: bloom | dry | exit");
+    }
+
+    Console.WriteLine("Size: " + tree.size + " | Foliage: " + tree.foliage);
+}
---
+++ Tree/Tree/Tree.cs
@@ -0,0 +1,34 @@
+﻿namespace TreeNamespace;
+
+public class Tree
+{
+    public int size;
+    public string foliage;
+    public string[] foliageLevels = { "Too much", "Not much", "None" };
+
+    public Tree()
+    {
+        size = 100;
+        foliage = "Too much";
+    }
+
+    public void Bloom()
+    {
+        size += 10;
+        Console.WriteLine("The new size is " + size + ".");
+    }
+
+    public void Dry()
+    {
+        if (foliage == "Too much")
+        {
+            foliage = "Not much";
+        }
+        else if (foliage == "Not much")
+        {
+            foliage = "None";
+        }
+
+        Console.WriteLine("The new foliage is " + foliage);
+    }
+}
