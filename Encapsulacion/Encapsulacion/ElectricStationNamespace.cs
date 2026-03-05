782dcdd8c2e56f54478039d626abe7498d0912ba
CesarSanza <cesar.sanchezar@outlook.com>
2026-03-05 12:19:43 -0600

    trabajo de miercoles

---
+++ Encapsulacion/Encapsulacion/ElectricStationNamespace.cs
@@ -0,0 +1,50 @@
+﻿namespace Encapsulacion;
+
+public class ElectricStation
+{
+    private string name;
+    private int charge;
+    private string password;
+
+    public ElectricStation()
+    {
+        name = "Electric Station";
+        charge = 0;
+        password = "1234";
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetName(string newName, string inputPassword)
+    {
+        if (inputPassword != password)
+        {
+            Console.WriteLine("Error: Contraseña incorrecta.");
+            return;
+        }
+        name = newName;
+    }
+
+    public int GetCharge()
+    {
+        return charge;
+    }
+
+    public void SetCharge(int newCharge)
+    {
+        if (newCharge < 0)
+        {
+            Console.WriteLine("Error: Thr charge cannot be negative.");
+                               return;
+        }
+        if (newCharge > 100)
+        {
+            Console.WriteLine("Error: The charge cannot exceed 100.");
+            return;
+        }
+        charge = newCharge;
+    }
+}
---
+++ Encapsulacion/Encapsulacion/Program.cs
@@ -0,0 +1,55 @@
+﻿using Encapsulacion;
+
+ElectricStation station = new ElectricStation();
+
+Console.WriteLine(" Electric Station Manager ");
+Console.WriteLine("Commands: setcharge | setname | getname | getcharge | exit");
+
+while (true)
+{
+    Console.Write("Enter command: ");
+    string input = Console.ReadLine();
+
+    if (input == "exit")
+    {
+        Console.WriteLine("Exiting");
+        break;
+    }
+    else if (input == "setcharge")
+    {
+        Console.Write("Enter charge value: ");
+        string chargeInput = Console.ReadLine();
+
+        if (int.TryParse(chargeInput, out int newCharge))
+        {
+            station.SetCharge(newCharge);
+        }
+        else
+        {
+            Console.WriteLine("Error: Ingresa un número válido.");
+        }
+    }
+    else if (input == "setname")
+    {
+        Console.Write("Enter new name: ");
+        string newName = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        string pass = Console.ReadLine();
+
+        station.SetName(newName, pass);
+    }
+    else if (input == "getname")
+    {
+        Console.WriteLine("Station name: " + station.GetName());
+    }
+    else if (input == "getcharge")
+    {
+        Console.WriteLine("Current charge: " + station.GetCharge());
+    }
+    else
+    {
+        Console.WriteLine("Unknown command. Use: setcharge | setname | getname | getcharge | exit");
+    }
+    
+}
