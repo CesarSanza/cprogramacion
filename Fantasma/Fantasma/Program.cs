719458a9a58bb0678362257b029c67276f558086
CesarSanza <cesar.sanchezar@outlook.com>
2026-04-06 19:35:49 -0600

    tarea

---
+++ Fantasma/Fantasma/Program.cs
@@ -0,0 +1,12 @@
+﻿using Ghost;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        character character = new character();
+        character.Introduce();
+
+        Gost gost = new Gost(); 
+    }
+}
---
+++ Fantasma/Fantasma/character.cs
@@ -0,0 +1,30 @@
+﻿namespace Ghost;
+
+public class character
+{
+    protected string name;
+
+    public character()
+    {
+        Introduce();
+    }
+
+    public virtual void Introduce()
+    {
+        Console.WriteLine($"Hello, world!");
+    }
+}
+
+public class Gost:character
+{
+    public Gost()
+    {
+        Introduce();
+    }
+
+    public override void Introduce()
+    {
+        base.Introduce();
+        Console.WriteLine("Dont you dare screaming!");
+    }
+}
