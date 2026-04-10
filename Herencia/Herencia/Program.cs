e4248f6f36fd108c7001d798bbf8e7c90ca32edf
CesarSanza <cesar.sanchezar@outlook.com>
2026-04-10 15:24:01 -0600

    tarea

---
+++ Herencia/Herencia/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Herencia;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Number of doors: ");
+        int doors = int.Parse(Console.ReadLine());
+
+        Car car = new Car(doors);
+
+        Console.Write("How many times do you want to accelerate? ");
+        int acelerar = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < acelerar; i++)
+        {
+            car.Accelerate();
+        }
+
+        Console.Write("Do you want to use the brakes? (yes/no): ");
+        bool brake = Console.ReadLine() == "yes";
+        if (brake)
+        {
+            car.Break();
+            Console.WriteLine("The car has slowed down.");
+        }
+
+        bool crashed = car.Crash();
+        if (crashed)
+            Console.WriteLine("The car crashed");
+        else
+            Console.WriteLine("The car is still running.");
+
+        car.OpenTrunk();
+    }
+}
---
+++ Herencia/Herencia/Vehicle.cs
@@ -0,0 +1,55 @@
+﻿namespace Herencia;
+
+public class Vehicle
+{
+    
+    protected string brand;
+    protected int speed;
+
+    public Vehicle()
+    {
+        this.brand = "Aston Martin";
+        this.speed = 0;
+    }
+    
+    public void Accelerate()
+    {
+        speed += 40;
+        Console.WriteLine($"New speed {speed}");
+    }
+
+    public void Break()
+    {
+        speed -= 20;
+    }
+
+    public bool Crash()
+    {
+        if (speed > 100)
+        {
+            Random rnd = new Random();
+            int randNum = rnd.Next(0, 2);
+            return randNum == 1;
+        }
+        return false;
+    }
+}
+
+class Car : Vehicle
+{
+    private int doorsNum;
+
+    public Car(int doorsNum)
+        : base()
+    {
+        this.doorsNum = doorsNum;
+    }
+
+    public void OpenTrunk()
+    {
+        if (doorsNum % 2 != 0)
+            Console.WriteLine("The trunk is open");
+        else
+            Console.WriteLine("This car has no trunk");
+    }
+}
